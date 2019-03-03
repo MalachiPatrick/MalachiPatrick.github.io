e76fd8c236c10a86c59133baad888b22ac88db92
Malachi Patrick <mal3dprinting@gmail.com>
2019-03-03 11:42:07 -0500

    Update TestScoutingApp.js

--- TestScoutingApp.js
+++ TestScoutingApp.js
@@ -1,6 +1,4 @@
 function myFunction(){
 var newWindow = window.open("", "_self");
-newWindow.document.write("<form><input type = \"button\" value = \"Hab Lvl 1\" style = \"width: 80%\"></form> ")
-newWindow.document.write("<form><input type = \"button\" value = \"Hab Lvl 1\" style = \"width: 20%\"></form> ")
-
+newWindow.document.write("<form><input type = \"button\" value = \"Hab Lvl 1\" style = \"width: 50%\"><input type = \"button\" value = \"Hab Lvl 1\" style = \"width: 20%\"></form> ")
 }
