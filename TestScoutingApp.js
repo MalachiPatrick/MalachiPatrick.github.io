46051c57bb92e52b1b73773278e0943b147e4e5e
Malachi Patrick <mal3dprinting@gmail.com>
2019-03-03 11:24:03 -0500

    Fixed

--- TestScoutingApp.js
+++ TestScoutingApp.js
@@ -1,4 +1,4 @@
 function myFunction(){
 var newWindow = window.open("", "_self");
-newWindow.document.write("<p>Hello there</p>")
+newWindow.document.write("<div class \"group\" id = \"habline\" style = \"width: 100%; height = 8.333%;\">")
 }
