628bd6461cf1c267d7fec662820032c88490dae9
Malachi Patrick <mal3dprinting@gmail.com>
2019-03-03 12:01:24 -0500

    Update TestScoutingApp.js

--- TestScoutingApp.js
+++ TestScoutingApp.js
@@ -1,4 +1,21 @@
 function myFunction(){
 var newWindow = window.open("", "_self");
+newWindow.document.write("startTimer()")
 newWindow.document.write("<form><input type = \"button\" value = \"Hab Lvl 1\" style = \"width: 50%\"><input type = \"button\" value = \"Hab Lvl 2\" style = \"width: 50%\"></form> ")
+}
+function startTimer(){
+  var timer = duration, minutes, seconds;
+  setInterval(function(){
+    minutes = parseInt(timer / 60,10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if(--timer < 0){
+      timer = duration;
+    }
+  }, 1000);
 }
