493ffdfcf938ffd8f6b2ee12b4aea94150ae86d1
Malachi Patrick <mal3dprinting@gmail.com>
2019-03-03 11:13:37 -0500

    Added/Fixed

--- TestScoutingApp.js
+++ TestScoutingApp.js
@@ -1,4 +1,4 @@
 function myFunction(){
 var newWindow = window.open("", "_self");
-newWindow.document.write("<input class =\"form form-control\" type = \"number\" placeholder = \"Did they score\">")
+newWindow.document.write("<p>Hello there</p>")
 }
