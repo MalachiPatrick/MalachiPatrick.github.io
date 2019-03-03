9fdeb665104fa2911cef706c266bfefe06c06083
Malachi Patrick <mal3dprinting@gmail.com>
2019-03-03 11:30:54 -0500

    Update TestScoutingApp.js

--- TestScoutingApp.js
+++ TestScoutingApp.js
@@ -1,5 +1,4 @@
 function myFunction(){
 var newWindow = window.open("", "_self");
-newWindow.document.write("<div class =\"group\" id = \"habline\" style = \"width: 100%; height = 8.333%;\">")
 newWindow.document.write("<div class = \"input btn btn-info\" id =\"habLine1\" style = \"height = 100%; width = 70%;\">Crossed Hab Level 1</div>")
 }
